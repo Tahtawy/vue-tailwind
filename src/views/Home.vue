c157a23e3c06ea365d4c65007fe050a57aeec7e2
Tahtawy <mght29@gmail.com>
2019-10-04 18:12:50 +0200

    inits project

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <button class="btn btn-blue">
+			Button
+		</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+	
+</style>
